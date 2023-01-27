b64762b972c163485636960b82e861a564e72a79
Sparsh <77487266+iSparshP@users.noreply.github.com>
2023-01-27 08:27:48 +0530

    Add files via upload

---
+++ TAXSAVING.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d",&t);
+    while(t>0)
+    {
+        int x,y;
+        cin>>x>>y;
+        cout<<x-y<<endl;
+        t--;
+    }
+	return 0;
+}
